a87af41f231ba05274bc0f87fa62d0a92827cd1c
Capot172 <vuhuuhoang1702@gmail.com>
2022-04-01 09:39:47 +0700

    Initial Commit

--- app/src/main/java/com/example/bottom_menu/CaptureAct.java
+++
@@ -1,6 +0,0 @@
-package com.example.bottom_menu;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureAct extends CaptureActivity {
-}
--- app/src/main/java/com/example/bottom_menu/ScanFragment.java
+++ app/src/main/java/com/example/bottom_menu/ScanFragment.java
@@ -1,50 +1,71 @@
 package com.example.bottom_menu;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResultCaller;
-import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.core.UseCaseGroup;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.common.util.concurrent.ListenableFuture;
 
-import com.journeyapps.barcodescanner.ScanContract;
-import com.journeyapps.barcodescanner.ScanOptions;
+import java.util.concurrent.ExecutionException;
 
 public class ScanFragment extends Fragment {
 
-
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    public PreviewView previewView ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        scanCode();
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+        previewView = view.findViewById(R.id.previewView);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity());
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(requireActivity()));
+        return view;
     }
 
-    private void scanCode(){
-        ScanOptions options = new ScanOptions();
-        options.setBeepEnabled(true);
-        options.setOrientationLocked(true);
-        options.setCaptureActivity(CaptureAct.class);
-        barLauncher.launch(options);
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+
+        previewView.setImplementationMode(PreviewView.ImplementationMode.COMPATIBLE);
+
+        Preview preview = new Preview.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3).
+                setTargetRotation(previewView.getDisplay().getRotation())
+                .build();
+
+
+        CameraSelector cameraSelector =
+                new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        UseCaseGroup useCaseGroup = new UseCaseGroup.Builder()
+                .addUseCase(preview)
+                .build();
+
+
+        cameraProvider.bindToLifecycle(this, cameraSelector,preview);
+
     }
 
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->{
-        if (result.getContents()  != null){
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle("Result");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();
-                }
-            }).show();
-        }
-    });
 
 }
